3b0f81c46acfe6d456b07605a600b724ec8ca24b
mongo928 <132264693+mongo928@users.noreply.github.com>
2023-09-19 15:21:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iwhiwh/SEND-ALL-FILE-BOT /SEND-ALL-FILE-BOT
+  git clone https://github.com/mongo928/SEND-ALL-FILE-BOT /SEND-ALL-FILE-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SEND-ALL-FILE-BOT
